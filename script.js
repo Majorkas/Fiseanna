746576ac892ad3b5864dc7fcbad52a37ffea8011
Aaron Richards <aaronrichards46@gmail.com>
2025-07-03 11:12:06 +0100

    added function for a hover effect on each img to display on the main img

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 const menuButton = document.getElementsByClassName('menu-button')
 let mainNav = document.getElementById('main-nav')
 let mottoTxt = document.getElementById('motto-text')
+const IMG_ID_SRC = {
+  'Main-img': "images/lake.jpg",
+  'smaller-img1': "images/black-castle.jpg",
+  'smaller-img2': "images/faces.jpg",
+  'smaller-img3': "images/rock.jpg",
+  'smaller-img4': "images/bike.jpeg",
+}
+
+let mainImg = document.getElementById('Main-img');
+function imgSwap(imgSrc) {
+  mainImg.src = imgSrc;
+
+
+}
+function imgSwapBack() {
+  mainImg.src = "images/lake.jpg"
+
+}
+
+
 function init() {
   menuButton[0].addEventListener('click', () => {
     mainNav.classList.toggle('hidden');
     mottoTxt.classList.toggle('hidden');
 
   });
+  Object.entries(IMG_ID_SRC).forEach(([imgId, imgSrc]) => {
+    const imgElem = document.getElementById(imgId);
+    imgElem.addEventListener("mouseover", () => imgSwap(imgSrc) );
+    imgElem.addEventListener("mouseout", imgSwapBack);
+  });
 
 }
-
-
-
-
-
 init()
